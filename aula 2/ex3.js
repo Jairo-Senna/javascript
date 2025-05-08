e64ffd846b05b6f9cb01aa54266c4890a8685207
Jairo-Senna <142929627+Jairo-Senna@users.noreply.github.com>
2025-05-08 18:37:09 -0300

    Delete aula 2 directory

--- aula 2/ex3.js
+++
@@ -1,5 +0,0 @@
-let idade = parseInt(prompt("Informe sua idade:"));
-
-let permitido = idade >= 18 ? "Entrada permitida" : "Entrada negada";
-
-alert(permitido);
--- aula 2/ex5.js
+++
@@ -1,9 +0,0 @@
-// Perguntar ao usuário se ele deseja excluir a conta
-let confirmarExclusao = confirm("Você deseja excluir sua conta?");
-
-// Verificar a resposta do usuário
-if (confirmarExclusao) {
-    alert("Conta excluída com sucesso");
-} else {
-    alert("Ação cancelada pelo usuário");
-}
